dd767adfff38cd10032212e238fccad2daad3fb5
Luis García Gómez <garciagomezluis.94@gmail.com>
2023-01-22 00:44:24 -0300

    chore: add hardhat setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
     extends: ['@luchio/eslint-config-base-ts'],
+    env: {
+        mocha: true,
+        node: true,
+    },
+    rules: {
+        'import/no-extraneous-dependencies': 'off',
+    },
 };
---
+++ scripts/deploy.ts
@@ -0,0 +1,15 @@
+import { ethers } from 'hardhat';
+
+async function main() {
+    const Lock = await ethers.getContractFactory('Lock');
+    const lock = await Lock.deploy();
+
+    await lock.deployed();
+
+    console.log(`Lock deployed to ${lock.address}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
